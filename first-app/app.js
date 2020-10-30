e4011e94f03060926546886209d2181c08360ed4
marcos <marcoslemeborbafilho@gmail.com>
2020-10-30 18:10:16 -0300

    Lesson 16 Path module

--- first-app/app.js
+++ first-app/app.js
@@ -1,10 +1,12 @@
+const path = require('path'); //Load a build in module.
+
+let pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
 const log = require('./logger');
 
-
-
 log('message');
-
-
 
 function sayHello(name) {
     console.log('Hello ' + name);

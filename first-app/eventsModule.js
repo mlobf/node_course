b00c8cc2572b76f39f7b3735ac04877caccef219
marcos <marcoslemeborbafilho@gmail.com>
2020-11-04 23:51:24 -0300

    commit

--- first-app/eventsModule.js
+++ first-app/eventsModule.js
@@ -8,14 +8,13 @@
 const EventEmitter = require('events');
 
 // Now lets create an instance of this class.
-const emitter = new EventEmitter();//This is the an object.
+const emitter = new EventEmitter(); //This is the an object.
 
 //Register a listener.
-emitter.on('messageLogged', function(){
+emitter.on('messageLogged', function () {
     console.log('Listener called');
 });
 
 //Raise an event
 // Important, this Raise must be places after the listener. The Listerner will be called recursively.
 emitter.emit('messageLogged'); // Emit means "make a noise - signalling a event is happening"
-

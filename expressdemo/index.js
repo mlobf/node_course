3aff5ba92ecb82b7912d86fda6b76472fa724365
marcos <marcoslemeborbafilho@gmail.com>
2020-12-01 21:50:01 -0300

    Has postman on REST

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -10,14 +10,29 @@ const express = require('express');
 
 const app = express();
 
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-    {id: 4, name: 'course4'},
+app.use(express.json());
+
+const courses = [{
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    },
+    {
+        id: 4,
+        name: 'course4'
+    },
 ];
 
+
 // This method takes two arguements, first url path, second (request, response)
+
 app.get('/', (req, res) => {
     res.send('Hello Word !!!!!!');
 });
@@ -38,13 +53,31 @@ app.delete();
     res.send(req.params);
 });
 */
-app.post('/api/courses',(req, res)=> {
 
+// Now its time to make https POST requests.
+app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 status code , that means Bas Request
+        res.status(400).send('Name is required and shoud be minimum 3 characters');
+        // This validation code shoud be placed in the beginng of  your route hangler
+        // So the node user has the option to user a node package that helps on get this done.
+        // npm joi  
+        return;
+    }
+    // Now lets create a new course object 
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+
+    };
+    courses.push(course);
+    res.send(course);
 });
 
+
 app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c =>c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.')//404 - Object not found
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.') //404 - Object not found
     res.send(course);
 });
 

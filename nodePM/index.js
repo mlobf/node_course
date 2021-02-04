f51f6bc38d70d37ffc6fb0b16cd0f62beeee71f0
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-04 17:14:46 -0300

    ExpressJS

--- nodePM/index.js
+++ nodePM/index.js
@@ -1,34 +1,36 @@
-// Lets have some fun 
-console.log("")
+// Lets have some fun
+console.log("");
 
-console.log('To install a npm package you Shall Do This :')
-console.log("")
-console.log(" npm i nameOfPackage")
-console.log("")
+console.log("To install a npm package you Shall Do This :");
+console.log("");
+console.log(" npm i nameOfPackage");
+console.log("");
 
-let _ = require('./npm-demo/node_modules/underscore');
+let x = require("./npm-demo/node_modules/underscore");
 
-// Core module 
+// Core module
 // File or Folder
 // node_modules
 
-let result = _.contains([1,2,3], 2);
+let result = .contains([1, 2, 3], 2);
 
 console.log(result);
 console.log("=======>");
-console.log('To install a specif version just do:')
-console.log('npm i nameOfPackege@versionNumber')
+console.log("To install a specif version just do:");
+console.log("npm i nameOfPackege@versionNumber");
 
-console.log(' ')
-// How to hide the node modules I should put on git ignore 
+console.log(" ");
+// How to hide the node modules I should put on git ignore
 
-console.log('To install a dependencies to Dev enviroment, lets find out below ...')
-console.log(' ')
+console.log(
+	"To install a dependencies to Dev enviroment, lets find out below ..."
+);
+console.log(" ");
 
 console.log("npm i jshint --save-dev");
 
-console.log(' ')
-console.log('To unistall you must Do as described below.......')
-console.log(' ')
+console.log(" ");
+console.log("To unistall you must Do as described below.......");
+console.log(" ");
 
-console.log('npm un mongoose')
+console.log("npm un mongoose");

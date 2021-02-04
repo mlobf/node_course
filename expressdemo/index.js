9adf9b474e8623e88935ae7b6955f1ea04f7c4c9
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-04 17:41:16 -0300

    npm install express

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -1,7 +1,6 @@
-const Joi = require('joi');
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const app = express();
-
 
 // Lets rock on web!!!
 
@@ -13,33 +12,33 @@ const app = express();
 // Enviroment Varablies
 app.use(express.json());
 
-const courses = [{
-        id: 1,
-        name: 'course1'
-    },
-    {
-        id: 2,
-        name: 'course2'
-    },
-    {
-        id: 3,
-        name: 'course3'
-    },
-    {
-        id: 4,
-        name: 'course4'
-    },
+const courses = [
+	{
+		id: 1,
+		name: "course1",
+	},
+	{
+		id: 2,
+		name: "course2",
+	},
+	{
+		id: 3,
+		name: "course3",
+	},
+	{
+		id: 4,
+		name: "course4",
+	},
 ];
-
 
 // This method takes two arguements, first url path, second (request, response)
 
-app.get('/', (req, res) => {
-    res.send('Hello Word !!!!!!');
+app.get("/", (req, res) => {
+	res.send("Hello Word !!!!!!");
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get("/api/courses", (req, res) => {
+	res.send(courses);
 });
 
 /*
@@ -56,42 +55,39 @@ app.delete();
 */
 
 // Now its time to make https POST requests.
-app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+app.post("/api/courses", (req, res) => {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
 
-    const result = Joi.validate(req.body, schema);
-    console.log(result);
+	const result = Joi.validate(req.body, schema);
+	console.log(result);
 
-
-
-    if (!req.body.name || req.body.name.length < 3) {
-        // 400 status code , that means Bas Request
-        res.status(400).send('Name is required and shoud be minimum 3 characters');
-        // This validation code shoud be placed in the beginng of  your route hangler
-        // So the node user has the option to user a node package that helps on get this done.
-        // npm joi  
-        return;
-    }
-    // Now lets create a new course object 
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-
-    };
-    courses.push(course);
-    res.send(course);
+	if (!req.body.name || req.body.name.length < 3) {
+		// 400 status code , that means Bas Request
+		res.status(400).send("Name is required and shoud be minimum 3 characters");
+		// This validation code shoud be placed in the beginng of  your route hangler
+		// So the node user has the option to user a node package that helps on get this done.
+		// npm joi
+		return;
+	}
+	// Now lets create a new course object
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name,
+	};
+	courses.push(course);
+	res.send(course);
 });
 
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.') //404 - Object not found
-    res.send(course);
+app.get("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course)
+		res.status(404).send("The course with the given ID was not found."); //404 - Object not found
+	res.send(course);
 });
 
-// Query parameters are stored 
+// Query parameters are stored
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));

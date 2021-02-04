c113e5d3fa01721344003f73131314574e91999d
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-04 19:18:39 -0300

    add  :id to express.js

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -3,13 +3,25 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const courses = [
+	{
+		id: 1,
+		name: "course1",
+		id: 2,
+		name: "course2",
+	},
+];
 
 app.get("/", (req, res) => {
 	res.send("Hello by Word!");
 });
 
 app.get("/api/courses", (req, res) => {
-	res.send([1, 2, 3]);
+	res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
 });
 
 // PORT

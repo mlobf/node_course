22bf7b9eb53217385d76f63a4adc1bcf97850fe2
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-05 09:45:32 -0300

    handle http post requests on node

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -3,12 +3,21 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+
 const courses = [
 	{
 		id: 1,
 		name: "course1",
+	},
+	{
 		id: 2,
 		name: "course2",
+	},
+	{
+		id: 3,
+		name: "course3",
 	},
 ];
 
@@ -22,9 +31,19 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course)
+		res.status(404).send("The course with the given ID was not found");
+	res.send(course);
 });
 
-// PORT
-const port = process.env.PORT || 3000;
+app.post("/api/courses", (req, res) => {
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name,
+	};
+	courses.push(course);
+	res.send(course);
+});
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));

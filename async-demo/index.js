5e93cba9d7329ef60e9a1c1abfb0c25a88660275
mlobf <marcoslemeborbafilho@gmail.com>
2021-03-07 22:11:05 -0300

    Lesson 71 Pattens for Dealing With Async Code

---
+++ async-demo/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Before");
+const user = getUser(1);
+console.log(user);
+console.log("After");
+function getUser(id) {
+	setTimeout(() => {
+		console.log("Reading a user from a database...");
+		return { id: id, gitHubUserName: "mosh" };
+	});
+}
+
+// Callbacks
+// Promises
+// Async/await
+
